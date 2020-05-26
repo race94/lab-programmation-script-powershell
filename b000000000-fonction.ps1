d5e65b3d1cd23bd459ae462f869b09fbf1177d7f
Mouss <mouss@MacBook-Pro-de-Mouss.local>
2020-05-26 21:59:29 +0200

    Tutoriel complété

--- b000000000-fonction.ps1
+++ b000000000-fonction.ps1
@@ -14,8 +14,17 @@
 # Definition de la fonction
 function Stagiaire {
 
+    [CmdletBinding()]
+    param (
+        [String]$personneNom,
+        [Int]$personneAge
+)
+
+# message de bienvenue 
+BEGIN {Write-Verbose "Début du script"}
+PROCESS { "Bonjour {0} ! Tu as {1} ans." -F $personneNom, $personneAge }
+END {Write-Verbose "Fin du script"}
 }
 
 # Appel de la fonction
-Stagiaire
-
+Stagiaire "Pascal Siakam" 26  -verbose
